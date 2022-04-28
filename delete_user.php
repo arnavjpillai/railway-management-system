537bd110a10da3ef28abba9e6672828436336bc4
YashVardhanSingh01 <86322402+YashVardhanSingh01@users.noreply.github.com>
2022-04-28 17:19:02 +0530

    Update delete_user.php

--- delete_user.php
+++ delete_user.php
@@ -1,9 +1,35 @@
 <html>
-<body style=" background-image: url(adminlogin.jpeg);
-    height: 100%; 
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;">
+<style>
+a {
+    text-decoration: none;
+}
+a:link {
+    color: #ffd240;
+}
+a:visited {
+    color: #00ddff;
+}
+a:hover {
+    color: red;
+}
+a:active {
+    color: blue;
+
+
+}
+
+</style>
+<body style=" 
+box-sizing: border-box;
+margin: 0;
+padding: 0;
+background-image: url(train4.jpg);
+height: 100%; 
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;">
+
+<div style="background-color: rgba(0, 0, 0, 0.404);backdrop-filter: blur(5px);max-width: 100%;margin: auto; padding: 5rem;margin-top: 12rem;color: whitesmoke;">
 
 <?php
 
@@ -22,7 +48,7 @@ echo "<br> <a href=\"http://localhost/railway/admin_login.php\">Go Back to Admin
 
 $conn->close();
 ?>
-
+   </div>
 </body>
 </html>
 
