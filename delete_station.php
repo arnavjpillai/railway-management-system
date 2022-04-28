65852edc7063cc3e20e761222c0ef437b1028728
arnavjpillai <99252308+arnavjpillai@users.noreply.github.com>
2022-04-28 14:43:55 +0530

    Update delete_station.php

--- delete_station.php
+++ delete_station.php
@@ -1,30 +1,58 @@
 <html>
-<body style=" background-image: url(adminlogin.jpeg);
-    height: 100%; 
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;">
+<style>
+    a {
+        text-decoration: none;
+    }
 
-<?php
+    a:link {
+        color: #ffd240;
+    }
 
-require "db.php";
+    a:visited {
+        color: #00ddff;
+    }
 
-$sql = "DELETE from station where id= ('".$_GET["id"]."')";
-echo $_GET["id"];
+    a:hover {
+        color: red;
+    }
 
-if ($conn->query($sql) === TRUE) {
-    echo " '".$_POST["sname"]."': Record deleted successfully";
-} else {
-    echo "Error:" . $conn->error;
-}
-
-echo "<br> <a href=\"http://localhost/railway/admin_login.php\">Go Back to Admin Menu!!!</a> ";
-
-$conn->close();
-?>
+    a:active {
+        color: blue;
 
 
+    }
+</style>
+
+<body style=" 
+box-sizing: border-box;
+margin: 0;
+padding: 0;
+background-image: url(train4.jpg);
+height: 100%; 
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;">
+
+    <div style="background-color: rgba(0, 0, 0, 0.404);backdrop-filter: blur(5px);max-width: 100%;margin: auto; padding: 5rem;margin-top: 12rem;color: whitesmoke;">
+
+        <?php
+
+        require "db.php";
+
+        $query = "DELETE FROM station WHERE st_id={$_GET["id"]}";
+
+        if (mysqli_query($conn, $query)) {
+            echo " Record deleted successfully";
+        } else {
+            echo "Error:" . mysqli_error($conn);
+        }
+
+        echo "<br> <a href=\"http://localhost/railway/admin_login.php\">Go Back to Admin Menu!!!</a> ";
+
+        $conn->close();
+        ?>
+
+    </div>
 </body>
+
 </html>
-
-
