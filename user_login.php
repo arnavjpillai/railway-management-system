96055689c49cf2294d552fbac9a76d03a70849e6
YashVardhanSingh01 <86322402+YashVardhanSingh01@users.noreply.github.com>
2022-04-28 17:41:19 +0530

    Update user_login.php

--- user_login.php
+++ user_login.php
@@ -1,10 +1,36 @@
 <html>
+<style>
+a {
+    text-decoration: none;
+}
+a:link {
+    color: #ffd240;
+}
+a:visited {
+    color: #00ddff;
+}
+a:hover {
+    color: red;
+}
+a:active {
+    color: blue;
 
-<body style=" background-image: url(userlogin.png);
-    height: 100%; 
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;">
+
+}
+
+</style>
+<body style=" 
+box-sizing: border-box;
+margin: 0;
+padding: 0;
+background-image: url(train2.jpeg);
+height: 100%; 
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;">
+
+<div style="background-color: rgba(0, 0, 0, 0.404);backdrop-filter: blur(5px);max-width: 100%;margin: auto; padding: 5rem;margin-top: 12rem;color: whitesmoke;">
+
 
 	<?php
 
@@ -51,7 +77,7 @@
 	$conn->close();
 
 	?>
-
+</div>
 </body>
 
 </html>
