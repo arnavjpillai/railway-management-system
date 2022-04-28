042eb60ac9d973f75842cdbd1c890c44b27050a3
YashVardhanSingh01 <86322402+YashVardhanSingh01@users.noreply.github.com>
2022-04-28 17:34:37 +0530

    Update insert_into_train_2.php

--- insert_into_train_2.php
+++ insert_into_train_2.php
@@ -1,9 +1,36 @@
 <html>
-<body style=" background-image: url(adminlogin.jpeg);
-    height: 100%; 
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;">
+<style>
+a {
+    text-decoration: none;
+}
+a:link {
+    color: #ffd240;
+}
+a:visited {
+    color: #00ddff;
+}
+a:hover {
+    color: red;
+}
+a:active {
+    color: blue;
+
+
+}
+
+</style>
+<body style=" 
+box-sizing: border-box;
+margin: 0;
+padding: 0;
+background-image: url(train1.jpg);
+height: 100%; 
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;">
+
+<div style="background-color: rgba(0, 0, 0, 0.404);backdrop-filter: blur(5px);max-width: 100%;margin: auto; padding: 5rem;margin-top: 12rem;color: whitesmoke;">
+
 
 <?php
 
@@ -21,6 +48,6 @@ echo "<br> <a href=\"http://localhost/railway/admin_login_1.php\">Go Back to Adm
 
 $conn->close();
 ?>
-
+    </div>
 </body>
 </html>
