24301677dd99e99276d13e13fc139cd353def886
arnavjpillai <99252308+arnavjpillai@users.noreply.github.com>
2022-04-28 17:54:11 +0530

    Update show_users.php

--- show_users.php
+++ show_users.php
@@ -35,21 +35,19 @@ background-size: cover;">
 <?php
 
 require "db.php";
-
-$cdquery="SELECT * FROM user";
-$cdresult=mysqli_query($conn,$cdquery);
-echo "
+$query = "SELECT * FROM user";
+    $result = mysqli_query($conn, $query);
+    echo "
 <table>
 <thead><td>Id</td><td>Email_Id</td><td>Password</td><td>Mobile_no</td><td>Date_Of_Birth</td><td></td><td></td></thead>
 ";
 
-while ($cdrow=mysqli_fetch_array($cdresult)) 
-{
-	echo "
-<tr><td>".$cdrow[0]."</td><td>".$cdrow[1]."</td><td>".$cdrow[2]."</td><td>".$cdrow[3]."</td><td>".$cdrow[4]."</td><td><a href=\"http://localhost/railway/edit_user.php?id=".$cdrow[0]."\"><button>Edit</button></a></td><td><a href=\"http://localhost/railway/delete_user.php?id=".$cdrow[0]."\"><button>Delete</button></a></td></tr>
+    while ($row = mysqli_fetch_array($result)) {
+        echo "
+<tr><td>{$row[0]}</td><td>{$row[1]}</td><td>{$row[2]}</td><td>{$row[3]}</td><td>{$row[4]}</td><td><a href=\"http://localhost/railway/edit_user.php?id={$row[0]}\"><button>Edit</button></a></td><td><a href=\"http://localhost/railway/delete_user.php?id={$row[0]}\"><button>Delete</button></a></td></tr>
 ";
-}
-echo "</table>";
+    }
+    echo "</table>";
 
 echo " <br><a href=\"http://localhost/railway/new_user_form.html\"> Add New User </a><br> ";
 echo " <br><a href=\"http://localhost/railway/admin_login.php\">Go Back to Admin Menu!!!</a> ";
