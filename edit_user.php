1c197d26afd9d20841b30a96b5a1450e533b3e2c
YashVardhanSingh01 <86322402+YashVardhanSingh01@users.noreply.github.com>
2022-04-28 17:20:09 +0530

    Update edit_user.php

--- edit_user.php
+++ edit_user.php
@@ -1,9 +1,36 @@
 <html>
-<body style=" background-image: url(adminlogin.jpeg);
-    height: 100%; 
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;">
+<style>
+a {
+    text-decoration: none;
+}
+a:link {
+    color: #ffd240;
+}
+a:visited {
+    color: #00ddff;
+}
+a:hover {
+    color: red;
+}
+a:active {
+    color: blue;
+
+
+}
+
+</style>
+<body style=" 
+box-sizing: border-box;
+margin: 0;
+padding: 0;
+background-image: url(train4.jpg);
+height: 100%; 
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;">
+
+<div style="background-color: rgba(0, 0, 0, 0.404);backdrop-filter: blur(5px);max-width: 100%;margin: auto; padding: 5rem;margin-top: 12rem;color: whitesmoke;">
+
 
 
 <?php
@@ -45,7 +72,7 @@ echo " <br><a href=\"http://localhost/railway/admin_login_1.php\">Go Back to Adm
 $conn->close();
 ?>
 
-
+</div>
 </body>
 </html>
 
